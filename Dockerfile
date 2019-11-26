bc4f18e167c714bc7815092b3606ad2461d756bb
pranith563 <pranithreddy563@gmail.com>
2019-11-26 15:26:21 +0530

    Added docker files

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM tomcat
+
+COPY target/DataAnonymizer.war /usr/local/tomcat/webapps/DataAnonymizer.war
---
+++ docker_pull.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This is a comment!
+export PATH=/bin:/usr/bin:/usr/local/bin
+docker pull pranith563/data_anonymizer:webimg
+docker-compose up -d
